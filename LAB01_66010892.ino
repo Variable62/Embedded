714b1e2bf6c2f571b86f12562afd43c9a7ee2f54
Adisorn_Sommart_12 <157354861+Variable62@users.noreply.github.com>
2025-01-27 18:23:01 +0700

    Update LAB01_66010892.ino

--- LAB01_66010892.ino
+++ LAB01_66010892.ino
@@ -1,19 +1,20 @@
 #include <avr/io.h>
-int main(){
+#define F_CPU 16000000UL
+int main(void){
   DDRD = DDRD | (1<<PD4);//LED RED O/P
   DDRD = DDRD | (1<<PD5);//LED YELLOW O/P
   DDRD = DDRD | (1<<PD6);//LED GREEN O/P
   DDRD &= ~(1<<PD3);//SW1 0
-  while(1){
+  while(true){
     PORTD = PORTD | (1<<PD6);//LED GREEN ON
 
     if(!(PIND & (1<<PD3))){
       PORTD = PORTD & ~(1<<PD6);
       PORTD = PORTD | (1<<PD5);
-       __builtin_avr_delay_cycles(47000000);//3s
+       __builtin_avr_delay_cycles(48000000);//3s
       PORTD = PORTD & ~(1<<PD5);
       PORTD = PORTD | (1<<PD4);
-       __builtin_avr_delay_cycles(155000000);//10s
+       __builtin_avr_delay_cycles(160000000);//10s
       PORTD = PORTD & ~(1<<PD4); 
     }
   }
